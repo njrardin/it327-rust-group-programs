5cd3393b0634fbb600ca4374329fc2b243ae5118
Cameron Crone <camtommc@gmail.com>
2023-05-05 12:42:17 -0500

    implements hello_world program

---
+++ hello_world/target/debug/deps/colored-d65be7e79036010a.d
@@ -0,0 +1,10 @@
+/Users/cameroncrone/Desktop/2023 (Spring)/Concepts of Programming/hello_world/target/debug/deps/colored-d65be7e79036010a.rmeta: /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/lib.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/color.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/control.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/style.rs
+
+/Users/cameroncrone/Desktop/2023 (Spring)/Concepts of Programming/hello_world/target/debug/deps/libcolored-d65be7e79036010a.rlib: /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/lib.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/color.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/control.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/style.rs
+
+/Users/cameroncrone/Desktop/2023 (Spring)/Concepts of Programming/hello_world/target/debug/deps/colored-d65be7e79036010a.d: /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/lib.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/color.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/control.rs /Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/style.rs
+
+/Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/lib.rs:
+/Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/color.rs:
+/Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/control.rs:
+/Users/cameroncrone/.cargo/registry/src/github.com-1ecc6299db9ec823/colored-2.0.0/src/style.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/cameroncrone/it327-rust-group-programs/hello_world/target/debug/hello_world: /Users/cameroncrone/it327-rust-group-programs/hello_world/src/main.rs
